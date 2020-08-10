7c98a54a935b07bc5da8494ea2421dfae20a4f96
ihor nesterov <igornes190@gmail.com>
2020-08-10 22:26:36 +0300

    Added debug feature

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#define Debug
+using System;
+using System.Diagnostics;
 
 namespace ConsoleApp1
 {
@@ -7,66 +9,70 @@ namespace ConsoleApp1
         public static string[] Parse(string input)
         {
             int wordscount = 0;
-
             for (int i = 0; i < input.Length; i++)
             {
-                //Console.WriteLine("Index:" + i);
-                //Console.WriteLine("Char:" + input.Substring(i, 1));
+#if (Debug)  
+                Console.WriteLine("Index:" + i);
+                Console.WriteLine("Char:" + input.Substring(i, 1));
+#endif
                 if (input.Substring(i, 1) == " ")
                 {
                     wordscount++;
                 }
             }
-            //Console.WriteLine("wordscount:" + wordscount);
-            int[] startpoints = new int[wordscount];
+#if (Debug)
+            Console.WriteLine("wordscount:" + wordscount);
+#endif 
             int[] endpoints = new int[wordscount + 1];
             string[] output = new string[wordscount];
-            int x = 1;
+            int currentpoint = 1;
             endpoints[0] = 0;
 
             for (int k = 0; k < input.Length; k++)
             {
                 if (input.Substring(k, 1) == " ")
                 {
-                    endpoints[x] = k + 1;
-                    //Console.WriteLine("Point:" + k);
-                    //Console.WriteLine("Endd:" + endpoints[x]);
-                    x++;
+                    endpoints[currentpoint] = k + 1;
+#if (Debug)
+                    Console.WriteLine("Point:" + k);
+                    Console.WriteLine("Endd:" + endpoints[currentpoint]);
+#endif
+                    currentpoint++;
                 }
             }
-            /*
+
+#if (Debug)
             for(int i = 0; i < endpoints.Length; i++)
             {
                 Console.WriteLine("Ended:" + endpoints[i]);
             }
-            */
+#endif
             for (int i = 1; i < wordscount + 1; i++)
             {
                 output[i - 1] = input.Substring(endpoints[i - 1], endpoints[i] - endpoints[i - 1]);
-                //Console.WriteLine("Endpoint:" + endpoints[i - 1]);
-                //Console.WriteLine("String:" + output[i - 1]);
+#if (Debug)
+                Console.WriteLine("Endpoint:" + endpoints[i - 1]);
+                Console.WriteLine("String:" + output[i - 1]);
             }
 
-            /*for (int i = 0; i < wordscount; i++)
+            for (int i = 0; i < wordscount; i++)
             {
                 Console.WriteLine("parsed:" + output[i]);
             }
-            */
+#else
+            }
+#endif
             return output;
         }
 
         static void Main(string[] args)
         {
-            Student student = new Student();
-            string f = "";
-            for (int i = 0; i < f.Length; i++)
+            string[] test = Parse("Debich lol kek ");
+            for(int i = 0; i < test.Length; i++)
             {
-                f += i.ToString() + " ";
+                Console.WriteLine(test[i]);
             }
-            for(int i = 0; i < f.Length; i++)
-            {
-                Console.WriteLine(f[i]);
-            }
+
             Console.ReadLine();
             Console.WriteLine("Hello World!");
         }
