20a1cbe44b392930a3b56a19fa4569b13d0c3d0d
ihor nesterov <igornes190@gmail.com>
2020-08-11 20:49:53 +0300

    Get set methods deleted

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -10,68 +10,79 @@ namespace ConsoleApp1
         private int weight;
         private string name;
         public static int count;
-        public static int id;
+        public int unique_id;
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                if (Age > 0 && Age < 150)
+                {
+                    this.age = Age;
+                }
+            }
+        }
+
+        public int Weight
+        {
+            get
+            {
+                return this.weight;
+            }
+            set
+            {
+                if(Weight > 10 && Weight < 500)
+                {
+                    this.weight = Weight;
+                }
+            }
+
+        }
+
+        public string Name
+        {
+            get 
+            {
+                return this.name;
+            }
+            set
+            {
+                if(Name != null)
+                {
+                    this.name = Name;
+                }
+            }
+        }
 
         public Human()
         {
-            id = count;
-            SetAge(20);
-            SetWeight(70);
-            SetName("Dodik");
+            unique_id = count;
+            this.Age = 20;
+            this.Weight = 70;
+            this.Name = "Dodik";
             count++;
         }
 
         public Human(int age, int weight, string name)
         {
-            id = count;
-            SetAge(age);
-            SetWeight(weight);
-            SetName(name);
+            unique_id = count;
+            this.Age = age;
+            this.Weight = weight;
+            this.Name = name;
             count++;
-        }
-
-        public void SetAge(int age)
-        {
-            if (age > 0 && age < 150)
-            {
-                this.age = age;
-            }
-        }
-        public int GetAge()
-        {
-            return age;
-        }
-
-        public void SetWeight(int weight)
-        {
-            if (weight > 10 && age < 400)
-            {
-                this.weight = weight;
-            }
-        }
-        public int GetWeight()
-        {
-            return weight;
-        }
-
-        public void SetName(string name)
-        {
-            if (name != null)
-            {
-                this.name = name;
-            }
-        }
-        public string GetName()
-        {
-            return name;
         }
 
         public virtual string Display()
         {
             string output = "";
-            output += "Name: " + this.GetName();
-            output += "\nAge: " + this.GetAge();
-            output += "\nWeight: " + this.GetWeight();
+            output += "Name: " + this.Name;
+            output += "\nAge: " + this.Age;
+            output += "\nWeight: " + this.Weight;
             return output;
         }
 
@@ -79,13 +90,13 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello,write name)");
             string input = Console.ReadLine();
-            this.SetName(input);
+            this.Name = input;
             Console.WriteLine("Write weight");
             input = Console.ReadLine();
-            this.SetWeight(Int32.Parse(input));
+            this.Weight = Int32.Parse(input);
             Console.WriteLine("Write age");
             input = Console.ReadLine();
-            this.SetAge(Int32.Parse(input));
+            this.Age = Int32.Parse(input);
         }
     };
 
@@ -97,79 +108,77 @@ namespace ConsoleApp1
 
         public Student() : base()
         {
-            int[] mark = { 3 };
-            string[] subject = { "Default"};
-            this.Setmarks(mark);
-            this.SetSubjects(subject);
-            this.SetUniversityName("Default university name");
+            int[] marks = { 3 };
+            string[] subjects = { "Default"};
+            this.Marks = marks;
+            this.Subjects = subjects;
+            this.University_Name = "Default university name";
         }
 
         public Student(int age, int weight, string name, string univer_name) : base(age, weight, name)
         {
-            this.SetUniversityName(univer_name);
+            this.University_Name = univer_name;
         }
 
         public Student(int age, int weight, string name, string univer_name,string[] subjects,int[] marks) : this(age,weight,name,univer_name)
         {
-            this.SetSubjects(subjects);
-            this.Setmarks(marks);
+            this.Subjects = subjects;
+            this.Marks = marks;
         }
 
-        public void SetUniversityName(string univer_name)
+        public string University_Name
         {
-            if (univer_name != null)
+            get
             {
-                this.university_name = univer_name;
+               return this.university_name;
+            }
+            set
+            {
+                if(University_Name != null)
+                {
+                    this.university_name = University_Name;
+                }
             }
         }
-        public string GetUniversityName()
-        {
-            return this.university_name;
-        }
 
-        public void SetSubjects(string[] subjects)
+        public string[] Subjects
         {
-            if (subjects != null)
+            get
             {
-                this.subjects = subjects;
+                return this.subjects;
+            }
+            set
+            {
+                if (subjects != null)
+                {
+                    this.subjects = Subjects;
+                }
             }
         }
-        public string[] GetSubject()
-        {
-            return this.subjects;
-        }
 
-        public string GetSubject(int pos)
+        public int[] Marks
         {
-            return this.subjects[pos];
-        }
-
-        public int[] GetMarks()
-        {
-            return this.marks;
-        }
-
-        public int GetMarks(int pos)
-        {
-            return this.marks[pos];
-        }
-
-        public void Setmarks(int[] mark)
-        {
-            if (mark != null)
+            get
             {
-                this.marks = mark;
+                return this.marks;
+            }
+            set
+            {
+                if(Marks != null)
+                {
+                    this.marks = Marks;
+                }
             }
         }
 
         public override string Display()
         {
             string output = base.Display();
-            output += "\nUniversity name:" + GetUniversityName();
+            output += "\nUniversity name:" + University_Name;
             output += "\nMarks:\n";
-            for (int i = 0; i < this.marks.Length; i++)
+            for (int i = 0; i < this.Marks.Length; i++)
             {
-                output += this.GetSubject(i) + ":" + this.marks[i] + "\n";
+                output += this.Subjects[i] + ":" + this.Marks[i] + "\n";
             }
             return output;
         }
@@ -178,7 +187,7 @@ namespace ConsoleApp1
         {
             base.Write();
             Console.WriteLine("\nWrite university name");
-            this.SetUniversityName(Console.ReadLine());
+            this.University_Name = Console.ReadLine();
         }
 
         
